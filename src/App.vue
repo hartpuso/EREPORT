95325826fe6dec9c3c38329770a0426b1a1e49ac
hartpuso <pusongbatwo02@gmail.com>
2026-03-24 05:59:51 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Report.vue'
+</script>
+
+<template>
+  <HelloWorld />
+  <Report />
+  
+</template>
